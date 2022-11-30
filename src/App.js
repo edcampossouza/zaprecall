978a48330830c457f2845fe095ca1875b576d860
Eduardo Souza <edcampossouza@gmail.com>
2022-11-30 09:57:20 -0300

    added initial data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
+import Game from "./components/Game";
 import GlobalStyle from "./GlobalStyles";
+import { cards as initialCards } from "./data";
 
 function App() {
+  initialCards.forEach((card) => (card.played = false));
+  const [cards, setCards] = useState(initialCards);
   return (
     <>
       <GlobalStyle />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Game />
+      <p>{JSON.stringify(cards)}</p>
     </>
   );
 }
