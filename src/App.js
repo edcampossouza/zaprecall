3045b03d883f7cb605668e707201fb7d5b637052
Eduardo Souza <edcampossouza@gmail.com>
2022-11-30 09:43:47 -0300

    reset css with createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import GlobalStyle from "./GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </>
   );
 }
 
