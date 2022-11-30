1f30034d70e95acb297fcca99c53c5b6aa562612
Eduardo Souza <edcampossouza@gmail.com>
2022-11-30 10:08:53 -0300

    added the fonts and the title

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { useState } from "react";
 import Game from "./components/Game";
 import GlobalStyle from "./GlobalStyles";
 import { cards as initialCards } from "./data";
+import styled from "styled-components";
 
 function App() {
   initialCards.forEach((card) => (card.played = false));
   const [cards, setCards] = useState(initialCards);
   return (
-    <>
+    <Container>
       <GlobalStyle />
       <Game />
-      <p>{JSON.stringify(cards)}</p>
-    </>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  font-family: "Recursive", sans-serif;
+  background-color: #FB6B6B;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export default App;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,31 @@
-export default function Game(){
-    return (<>
-        zaprecall
-    </>)
+import { logRoles } from "@testing-library/react";
+import styled from "styled-components";
+import logo from "../assets/img/logo.png";
+
+export default function Game() {
+  return (
+    <>
+      <Title>
+        <img src={logo} />
+        <span>ZapRecall</span>
+      </Title>
+    </>
+  );
 }
+
+const Title = styled.h1`
+  font-family: "Righteous", cursive;
+  font-weight: 400;
+  font-size: 36px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  span {
+    width: 204px;
+    text-align: center;
+  }
+  img {
+    width: 52px;
+    height: 60px;
+  }
+`;
